43dc0bbb2f9fce3f766dd9246f12c49dc1e0f986
i-khadra <saeed.developer18@gmail.com>
2023-03-09 08:59:26 +0200

    add des order sorting

--- Sorting Algorithms/insertion-sort.cpp
+++ Sorting Algorithms/insertion-sort.cpp
@@ -4,15 +4,17 @@
 using namespace std;
 
 template <typename type>
-void insertion_sort(vector<type>& obj) {
+void insertion_sort(vector<type>& obj, bool des = false) {
     int n = obj.size();
     for (int i = 1; i < n; ++i) {
         int pos = i;
-        while (pos > 0 and obj[pos] < obj[pos-1])
+        bool DO = (obj[pos] > obj[pos-1] and des) or (obj[pos] < obj[pos-1] and !des);
+        while (DO) {
             swap(obj[pos], obj[pos-1]), pos--;
+            DO = pos > 0 and ((obj[pos] > obj[pos-1] and des) or (obj[pos] < obj[pos-1] and !des));
+        }
     }
 }
-
 
 ///////////////////// test & main /////////////////////
 
@@ -24,6 +26,9 @@ void print(const vector<int>& x) {
 void test() {
     vector<int> x = {2, 3, 1, 7, 4};
     insertion_sort(x);
+    print(x);
+
+    insertion_sort(x, true);
     print(x);
 
 }
