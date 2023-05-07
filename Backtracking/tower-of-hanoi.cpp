20ae4a490c0fb63129a0a4f3be3ca1c29cbc043d
Saeed Khadra <saeed.developer18@gmail.com>
2023-05-07 09:48:21 +0300

    add tower of hanoi problem

--- Backtracking/tower-of-hanoi.cpp
+++ Backtracking/tower-of-hanoi.cpp
@@ -10,66 +10,19 @@
 using namespace std;
 
 class Solution{
-    int N;
-    int original_to;
 
-    vector< stack<int> > rods;
-    bool done = false, first_call = true;
 public:
-    void init(int n, int from, int to) {
-        N = n;
-        original_to = to;
-
-        stack<int> tmp; tmp.push(0);
-        rods = vector< stack<int> >(4, tmp);
-        for (int i = n; i >= 1; --i) {
-            rods[from].push(i);
-        }
-    }
-
     long long toh(int n, int from, int to, int aux) {
-        if (first_call) {
-            init(n, from, to);
-            first_call = false;
-        }
-
-        if (rods[original_to].size() == N + 1)
-            done = true;
-        if (done or rods[from].top() == 0)
+        if (n == 0)
             return 0;
 
-        bool moved = false;
+        long long res = 1;
+        res += toh(n-1, from, aux, to); // move top (n-1) disks: from => aux
+        cout << "move disk " << n << " from rod " << from << " to rod " << to << endl;
+        res += toh(n-1, aux, to, from); // move the (n-1) disks that put previously: aux => to
 
-        long long all_paths = 0;
-        if (to != 0 and (rods[to].top() == 0 or rods[to].top() > rods[from].top())) { // from => to
-            cout << "move disk " << rods[from].top() << " from rod " << from << " to rod " << to << endl;
-
-            rods[to].push(rods[from].top()); // move to 'to'
-            rods[from].pop();
-            moved = true;
-
-            all_paths = toh(n, from, to, aux) +
-                    toh(n, to, 0, aux) + // use '0' to avoid moving repeatedly (i.e. from => to, to => from) .. which create INF loop
-                    toh(n, aux, to, from);
-        }
-        else if (aux != 0 and (rods[aux].top() == 0 or rods[aux].top() > rods[from].top())) { // from => aux
-            cout << "move disk " << rods[from].top() << " from rod " << from << " to rod " << aux << endl;
-
-            rods[aux].push(rods[from].top()); // move to 'aux'
-            rods[from].pop();
-            moved = true;
-
-            all_paths = toh(n, from, to, aux) +
-                    toh(n, aux, 0, to) +
-                    toh(n, to, from, aux);
-        }
-        else {
-            all_paths = toh(n, to, from, aux) + toh(n, aux, to, from);
-        }
-
-        return (moved + all_paths);
+        return res;
     }
-
 };
 
 int main() {
